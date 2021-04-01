ef321d997e98c7e5c5bde5ed6c32e1c73c071380
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-04-01 22:20:46 +0300

    add page personal page

--- main.py
+++ main.py
@@ -22,7 +22,14 @@ text_phrases = {
     'not_understand': ['Не понятно, попробуйте перефразировать',
                        'Что вы сказали?',
                        'Я вас не понимаю',
-                       'Правила почитайте']
+                       'Правила почитайте'],
+    'about': ['Должность: \n Директор, учитель информатики\n\n'
+              'Преподаваемые дисциплины: \n Информатика и ИКТ\n\n'
+              'Специальность: \n Учитель информатики, инженер по автоматизации химических производств \n\n'
+              'Квалификация: \nВысшая квалификационная категория\n\n'
+              'Общий стаж работы: 29 лет\n\n'
+              'Педагогический стаж: 25 лет\n\n'
+              'Кабинет: 410']
 }
 
 app = Flask(__name__)
@@ -116,6 +123,13 @@ def old_user(res, req, user_id):
             'answer') + '\n' + dop[1]
     elif wants == 'skill':
         res['response']['text'] = res['response']['tts'] = get_random_phrases('rules')
+    elif wants == 'about':
+        res['response']['text'] = res['response']['tts'] = get_random_phrases('about')
+        res['response']['card'] = {}
+        res['response']['card']['type'] = "BigImage"
+        res['response']['card']['image_id'] = '1652229/a754a8a6586ac69c482b'
+        res['response']['card']['title'] = 'Любич Геннадий Рувимович'
+        res['response']['card']['description'] = 'Директор МАОУ "Лицей №14" г. Тамбов'
     else:
         res['response']['text'] = res['response']['tts'] = get_random_phrases('not_understand')
 
@@ -128,7 +142,7 @@ def what_user_want(req, user_id):
     if any(i in tokens for i in ['далёкий']):
         user = sessionStorage.get_user(user_id)
         return user.last
-    if any(i in tokens for i in ['спрашивать', 'вопрос', 'любич']):
+    if any(i in tokens for i in ['спрашивать', 'вопрос']):
         if any(i in tokens for i in ['новое', 'последний']):
             user = sessionStorage.get_user(user_id)
             user.number_question_sprashivai = 1
@@ -136,6 +150,8 @@ def what_user_want(req, user_id):
         return 'sprashivai'
     if any(i in tokens for i in ['уметь', 'мочь', 'правило']):
         return 'skill'
+    if any(i in tokens for i in ['кто', 'он', 'такой', 'рассказать', 'любич']):
+        return 'about'
 
 
 if __name__ == '__main__':
