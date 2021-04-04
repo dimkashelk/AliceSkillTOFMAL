ed3fb7e6095f1369d3b59a49ad1c8dd05e9b136d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-04-04 17:08:59 +0300

    update session.py

--- session.py
+++ session.py
@@ -43,13 +43,21 @@ class Session:
             self.add_question(v[0], v[1], v[2])
 
     def add_questions(self, list_questions, reverse=True):
+        len_questions = 0
         if reverse:
             for i in list_questions[::-1]:
-                self.add_question(i[0], i[1], i[2])
+                try:
+                    self.add_question(i[0], i[1], i[2])
+                    len_questions += 1
+                except BaseException:
+                    pass
         else:
             for i in list_questions:
-                self.add_question(i[0], i[1], i[2])
-        len_questions = len(list_questions)
+                try:
+                    self.add_question(i[0], i[1], i[2])
+                    len_questions += 1
+                except BaseException:
+                    pass
         list_users = self.session.query(users.User).all()
         for user in list_users:
             user.number_question_sprashivai += len_questions
