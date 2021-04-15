0a288ed6697cd2b57a1e0329e104669a47348641
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-04-15 20:53:30 +0300

    update headers in update_questions.py

--- update_questions.py
+++ update_questions.py
@@ -34,27 +34,29 @@ headers = {
     'Connection': 'keep-alive',
     'Content-Length': '35',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Cookie': '_ym_uid=1596696468343546232; '
-              '_ga=GA1.2.1694673686.1596696468; '
-              'usersession=5063d8d29fcd51ac78a088ad86cd681bd3c2ff92d2f3bb491adcd77a56ed345c; '
-              '__gads=ID=165efdf3256d4c85-22d1a12df9b80001:T=1602685204:RT=1602685204:S=ALNI_MaDxYq4AXYX9Q0Ux7dAHl9f6xjg0Q; '
-              '_ym_d=1612538642; '
-              '_fbp=fb.1.1614417207106.1185161453; '
-              '_gid=GA1.2.1481418507.1616346835; '
-              'HWETZ=E77IPUIO; '
-              'cf_clearance=e3217de983e3f380f497792bc6ed62eeb0ab59db-1617525562-0-150; '
-              '__cfduid=dbb127f86bce222149f4eb39b8fe0b86f1617525562; '
+    'Cookie': '__cfduid=db6a69658ea1d926c8e80498af38c01981618507402; '
               'check=1; '
               'spr_c=4ca5a1ffe083926d9df7909623cd4b5a8952c6a1966da4ee59afe7f4eef3caa0; '
-              '_ym_isad=1; '
+              'HWETZ=E77IPUIO; '
+              '__cf_bm=6a4061b8a16f060ef42da1cf3c992da6b8198670-1618507426-1800-AdsPtk7ZR/skmpWBdNVGvY5WfV4zDO8DfZkbUQe2qxY6LTssrqL9sRvldUbwRByKWsWXZxM9C8kclU1q6K5Da7ysR7pqfQsaO21iZbgoyE8HQ2pmjAc33kxR1mxzP5m+DQ==; '
+              '_ym_uid=1618507438169224150; '
+              '_ym_d=1618507438; '
               '_ym_visorc=w; '
-              '__cf_bm=58d7b93c4b077516b1a48084d2368d76fc181e9a-1617530499-1800-ARA9OlqYe4Q1oNAhJ1CBRdRT6aJZRG/9iEEjQnd/cZS9EAFBZ3CGRKCN5OiWsdjY7tJPZeQgOegLNMHei4dRED1JOeprGpefBsz/R2XzyXoXEU6iRJU5jqk1Oj/WDbDjEA==; '
+              '_ym_isad=1; '
+              '__gads=ID=85faea34454aafb2-2252af8b17bb00d5:T=1618507442:RT=1618507442:S=ALNI_MZbomvRpp8MyA3lvZ9-izIQJPemtg; '
+              '_fbp=fb.1.1618507452167.1870448315; '
+              '_ga=GA1.2.1226048678.1618507438; '
+              '_gid=GA1.2.1255317375.1618507458; '
               '_gat_gtag_UA_70093472_2=1',
     'DNT': '1',
     'Host': 'sprashivai.ru',
     'Origin': 'http://sprashivai.ru',
     'Referer': 'http://sprashivai.ru/lubichgr',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 YaBrowser/21.2.4.165 Yowser/2.5 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/89.0.4389.86 '
+                  'YaBrowser/21.3.1.91 '
+                  'Yowser/2.5 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
 }
 session = Session()
